d0f3410ce4a30caceeaad0041549899242944812
MrLuis137 <lalemn2@gmail.com>
2019-06-06 00:02:04 -0600

    Socket

---
+++ src/cliente/Cliente.java
@@ -0,0 +1,57 @@
+package cliente;
+
+import java.util.ArrayList;
+import java.net.Socket;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Clase que realiza la conexión con el servidor.
+ * @autor Luis Diego Aleman
+ * @autor Valeria Quesada
+ * @autor Marlen Solano
+ * @version 16/5/2019
+ */
+public class Cliente{
+  private static Cliente cliente;
+  private static final int PUERTO = 64923; //Puerto para la conexion
+  private static final String HOST = "localhost"; //Host para la conexion
+  protected static String mensajeServidor;
+  protected static Socket cs; //Socket del servidor;
+  protected static ObjectOutputStream salidaCliente; //Flujo de datos de salida
+    
+  public Cliente getCliente(){
+    if(cliente == null){
+      cliente = new Cliente();
+    }
+    return cliente;
+  }
+  
+  /**
+   * constructor del cliente del juego
+   */ 
+  private Cliente(){
+  }
+    
+  /**
+   * Envia la solicitud e informacion en forma de arraylist
+   * @param solicitud
+   * @return objeto
+   */
+  public Object enviarInformacion(Object solicitud){
+    try{
+      cs = new Socket(HOST, PUERTO);
+      salidaCliente= new ObjectOutputStream(cs.getOutputStream());
+      salidaCliente.writeObject(solicitud);
+      ObjectInputStream entrada = new ObjectInputStream(cs.getInputStream());
+      Object soli  = entrada.readObject();
+      cs.close(); 
+      return soli;
+    }
+    catch(Exception e){
+      e.printStackTrace();
+      return false;
+    }
+  }
+}
